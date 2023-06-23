9f5eaec76c6475cc91a4f027c6de50c77ccd0272
phaleth <29838606+phaleth@users.noreply.github.com>
2023-06-23 22:27:58 +0200

    finish Games: Guessing Game exercise

---
+++ lib/games/guessing_game.ex
@@ -0,0 +1,24 @@
+defmodule Games.GuessingGame do
+  def play() do
+    correct_num = :rand.uniform(10)
+    guess(correct_num)
+  end
+
+  defp guess(correct_num) do
+    guess_num = prompt()
+
+    if guess_num === correct_num do
+      IO.puts("Correct!")
+    else
+      if guess_num > correct_num, do: IO.puts("Too High!"), else: IO.puts("Too Low!")
+      guess(correct_num)
+    end
+  end
+
+  defp prompt() do
+    IO.gets("Enter your guess: ")
+    |> to_string()
+    |> String.trim_trailing()
+    |> String.to_integer()
+  end
+end
